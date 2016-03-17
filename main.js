0f7d43bed027e27ece476df2e857132ba28f44ff
marleemaloy <marleeamaloy@gmail.com>
2016-03-17 15:32:25 -0700

    created a function that adds an active class to the active link

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-$(function(){
-  var $title = $(".title")
-  $title.on( "focusout", function(event){
-    $title.text("Where are you?");
-  });
+$(function() {
+    // this will get the full URL at the address bar
+
+    var $pageURL = window.location.href;
+
+    // passes on every anchor tag
+
+    $(".menu-list a").each(function() {
+        // checks if its the same on the address bar
+
+        if ($pageURL == (this.href)) {
+            $(this).closest("a").addClass("active");
+        }
+    });
 });
