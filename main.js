3b28175ac562ee6b4f27c16d072e77b5cd4cbc9d
marleemaloy <marleeamaloy@gmail.com>
2016-03-22 15:57:14 -0700

    added javascript to make the header disappear on scroll down and reappear on scroll up

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
 $(function() {
     // this will get the full URL at the address bar
-
     var $pageURL = window.location.href;
-
     // passes on every anchor tag
-
     $(".menu-list a").each(function() {
-        // checks if its the same on the address bar
-
+        // checks if the active link is the same as on the address bar
         if ($pageURL == (this.href)) {
             $(this).closest("a").addClass("active");
         }
+    // Hide Header on on scroll down
+      var didScroll;
+      var lastScrollTop = 0;
+      var delta = 5;
+      var navbarHeight = $('.home-menu').outerHeight();
+
+      $(window).scroll(function(event){
+          didScroll = true;
+      });
+
+      setInterval(function() {
+          if (didScroll) {
+              hasScrolled();
+              didScroll = false;
+          }
+      }, 250);
+
+      function hasScrolled() {
+          var st = $(this).scrollTop();
+
+          // Make sure they scroll more than delta
+          if(Math.abs(lastScrollTop - st) <= delta)
+              return;
+
+          // If they scrolled down and are past the navbar, add class .nav-up.
+          // This is necessary so you never see what is "behind" the navbar.
+          if (st > lastScrollTop && st > navbarHeight){
+              // Scroll Down
+              $('.home-menu').removeClass('nav-down').addClass('nav-up');
+          } else {
+              // Scroll Up
+              if(st + $(window).height() < $(document).height()) {
+                  $('.home-menu').removeClass('nav-up').addClass('nav-down');
+              }
+          }
+
+          lastScrollTop = st;
+      }
     });
-
-    // fades in content in hero headers
-    $(".hero-content").hide();
-    $(".hero-content").fadeIn();
-
-});
+  });
