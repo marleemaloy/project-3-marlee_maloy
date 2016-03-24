a0826d28545e6ec1cc56bd7d9509e1bd1b600bb3
marleemaloy <marleeamaloy@gmail.com>
2016-03-24 11:33:54 -0700

    added our latest project section

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ $(function() {
         if ($pageURL == (this.href)) {
             $(this).closest("a").addClass("active");
         }
-    // Hide Header on on scroll down
+    });
+
+
+
+
+    // Hide Header on on scroll down, show on scroll up
       var didScroll;
       var lastScrollTop = 0;
       var delta = 5;
@@ -16,10 +21,12 @@ $(function() {
       var $homeMenu = $('.home-menu');
       var $document = $(document);
 
+    //When the user scrolls, set didScroll to true
       $window.scroll(function(event){
           didScroll = true;
       });
-
+      // Checks every 250ms to see if didScroll
+      //is true and if it is, runs hasScrolled and resets didScroll to false
       setInterval(function() {
           if (didScroll) {
               hasScrolled();
@@ -27,20 +34,21 @@ $(function() {
           }
       }, 250);
 
+
       function hasScrolled() {
           var currentPosition = $(this).scrollTop();
 
-          // Make sure they scroll more than delta
+          // Makes sure they scroll more than delta
           if(Math.abs(lastScrollTop - currentPosition) <= delta)
               return;
 
-          // If they scrolled down and are past the navbar, add class .nav-up.
-          // This is necessary so you never see what is "behind" the navbar.
+          // This is necessary so you never see what is "behind" the navbar
+          // If user scrolls down and scrolls past the navbar, hide menu
           if (currentPosition > lastScrollTop && currentPosition > navbarHeight){
-              // Scroll Down
               $homeMenu.removeClass('nav-down').addClass('nav-up');
           } else {
-              // Scroll Up
+              // On scroll up, show menu
+              // This condition accounts for a bug on macs where you can scroll past the document.height
               if(currentPosition + $window.height() < $document.height()) {
                   $homeMenu.removeClass('nav-up').addClass('nav-down');
               }
@@ -48,5 +56,4 @@ $(function() {
 
           lastScrollTop = currentPosition;
       }
-    });
   });
