0a8bd729f301176422f80458c1b241b3c095d970
marleemaloy <marleeamaloy@gmail.com>
2016-03-25 12:10:42 -0700

    Revert "Committing to revert, something went wrong"
    This reverts commit 481b2d49ceb5f8195fc06647dd60aad2ddd80c3f.

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(function() {
-
-  // this will get the full URL at the address bar
-  var $pageURL = window.location.href;
-  // passes on every anchor tag
-  $(".menu-list a").each(function() {
-      // checks if the active link is the same as on the address bar
-      if ($pageURL == (this.href)) {
-          $(this).closest("a").addClass("active");
-      }
-  });
-
+    // this will get the full URL at the address bar
+    var $pageURL = window.location.href;
+    // passes on every anchor tag
+    $(".menu-list a").each(function() {
+        // checks if the active link is the same as on the address bar
+        if ($pageURL == (this.href)) {
+            $(this).closest("a").addClass("active");
+        }
+    });
 
 
-    // Hide Header on on scroll down
+
+
+    // Hide Header on on scroll down, show on scroll up
       var didScroll;
       var lastScrollTop = 0;
       var delta = 5;
@@ -21,10 +21,12 @@ $(function() {
       var $homeMenu = $('.home-menu');
       var $document = $(document);
 
+    //When the user scrolls, set didScroll to true
       $window.scroll(function(event){
           didScroll = true;
       });
-
+      // Checks every 250ms to see if didScroll
+      //is true and if it is, runs hasScrolled and resets didScroll to false
       setInterval(function() {
           if (didScroll) {
               hasScrolled();
@@ -32,25 +34,26 @@ $(function() {
           }
       }, 250);
 
+
       function hasScrolled() {
           var currentPosition = $(this).scrollTop();
 
-          // Make sure they scroll more than delta
+          // Makes sure they scroll more than delta
           if(Math.abs(lastScrollTop - currentPosition) <= delta)
               return;
 
-          // If they scrolled down and are past the navbar, add class .nav-up.
-          // This is necessary so you never see what is "behind" the navbar.
+          // This is necessary so you never see what is "behind" the navbar
+          // If user scrolls down and scrolls past the navbar, hide menu
           if (currentPosition > lastScrollTop && currentPosition > navbarHeight){
-              // Scroll Down
               $homeMenu.removeClass('nav-down').addClass('nav-up');
           } else {
-              // Scroll Up
+              // On scroll up, show menu
+              // This condition accounts for a bug on macs where you can scroll past the document.height
               if(currentPosition + $window.height() < $document.height()) {
                   $homeMenu.removeClass('nav-up').addClass('nav-down');
               }
           }
 
           lastScrollTop = currentPosition;
-      });
+      }
   });
